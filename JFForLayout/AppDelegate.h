795020dad8d9542e9784ce51f5e38bab03df15ff
xjf1990930 <xjf1990930@163.com>
2015-08-31 14:00:37 +0800

    this is the creations
    just create the versions

---
+++ JFForLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JFForLayout
+//
+//  Created by youle on 15/8/31.
+//  Copyright (c) 2015年 JFBoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JFForLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JFForLayout
+//
+//  Created by youle on 15/8/31.
+//  Copyright (c) 2015年 JFBoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
